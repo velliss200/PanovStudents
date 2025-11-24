e2a301c0831c3135e0c05520d27862b8918fab21
velliss200 <velliss200@gmail.com>
2025-11-24 23:15:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const AbtUs = document.getElementById("AboutUs");
+AbtUs.addEventListener("click", () => {
+  window.location.href = "AboutUs.html";
+});
+const Events = document.getElementById("Events");
+Events.addEventListener("click", () => {
+  window.location.href = "Events.html";
+});
+const Contacts = document.getElementById("Contacts");
+Contacts.addEventListener("click", () => {
+  window.location.href = "Contacts.html";
+});
+const main = document.getElementById("main");
+main.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
