842f04ef87546baf350a08917f1e967af934c43e
velliss200 <velliss200@gmail.com>
2026-01-25 22:05:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,52 +11,73 @@ const FBBtn = document.getElementById("FBBtn");
 const YtBtn = document.getElementById("YtBtn");
 const InstaBtn = document.getElementById("InstaBtn");
 
+if (FBBtn){
+  
 FBBtn.addEventListener("click", () => {
   window.location.href =
     "https://www.facebook.com/groups/440153730023022/?locale=ru_RU";
-});
-
+});}
+if (YtBtn)
 YtBtn.addEventListener("click", () => {
   window.location.href = "https://www.youtube.com/@OLIMPYPANOV";
-});
+});}
+
+if(InstaBtn){
 InstaBtn.addEventListener("click", () => {
   window.location.href =
     "https://www.instagram.com/olimpypanov?igsh=bGN1bmhjb3NkcjRy&utm_source=qr";
-});
+});}
 const AUText2 = document.getElementById("AUText2");
 Main.addEventListener("click", () => {
   window.location.href = "index.html";
   Main.classList.add("click");
 });
-
-AbtUs.addEventListener("click", () => {
+if(AbtUs)
+{AbtUs.addEventListener("click", () => {
   window.location.href = "AboutUs.html";
   AbtUs.classList.add("click");
-});
-
+});}
+if(Events){
 Events.addEventListener("click", () => {
   window.location.href = "Events.html";
   Events.classList.add("click");
-});
-
+});}
+if(Contacts){
 Contacts.addEventListener("click", () => {
   window.location.href = "Contacts.html";
   Contacts.classList.add("click");
-});
+});}
 
+
+if(ActivityEventsBtn){
 ActivityEventsBtn.addEventListener("click", () => {
   window.location.href = "Events.html";
   Events.classList.add("click");
-});
+});}
+
+
+if(ActivityEduBtn){
 ActivityEduBtn.addEventListener("click", () => {
   window.location.href = "Events.html";
   Events.classList.add("click");
-});
+});}
+
+
+
+
+
+if(ActivitySportBtn){
 ActivitySportBtn.addEventListener("click", () => {
   window.location.href = "Events.html";
   Events.classList.add("click");
-});
+});}
+
+
+
+if(ActivityCharityBtn){
 ActivityCharityBtn.addEventListener("click", () => {
   window.location.href = "AboutUs.html";
   Events.classList.add("click");
 });
+
+}
